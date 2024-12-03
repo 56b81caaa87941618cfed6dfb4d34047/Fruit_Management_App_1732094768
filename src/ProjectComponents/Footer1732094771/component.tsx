6a50aaa1a396b23b6942af09de0f52969367328f
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-12-03 15:21:19 +0100

    Update source code for component: Footer1732094771

--- src/ProjectComponents/Footer1732094771/component.tsx
+++ src/ProjectComponents/Footer1732094771/component.tsx
@@ -73,9 +73,9 @@ const ContractInteraction: React.FC = () => {
        caller: callerAddress
      });
   
-     const tx = await contract.addTrustedRelayer(callerAddress,  gasLimit: {
-      gasLimit: 1000000
-    });
+     const tx = await contract.addTrustedRelayer(callerAddress, {
+        gasLimit: 1000000
+      });
      await tx.wait();
    } catch (error) {
      console.error('Detailed error:', error);
